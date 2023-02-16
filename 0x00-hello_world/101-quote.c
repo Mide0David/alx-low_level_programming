0fc49bb0c53ff086eb694150af1ba805fcaeae0c
Mide <midebabalola7@gmail.com>
2023-02-16 13:56:43 -0800

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
  */
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-	return (1);
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = sizeof(message) - 1;
+    ssize_t written = write(2, message, len);
+    return (written == len) ? 1 : -1;
 }
