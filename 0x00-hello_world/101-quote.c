1ad13da79ec8e0f7955ee43b4e4400ecf648ce36
Mide <midebabalola7@gmail.com>
2023-02-16 14:08:26 -0800

    use write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,6 @@
  */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(message) - 1;
-    ssize_t written = write(2, message, len);
-    return (written == len) ? 1 : -1;
+    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+    return (1);
 }
