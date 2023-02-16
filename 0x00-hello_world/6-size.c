d5361d4c40d5d6bb9d175e96970594213cf4bcfc
Mide <midebabalola7@gmail.com>
2023-02-16 13:03:40 -0800

    add space after comma

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - start point to print the space size of each data type it the printf statment
+ * main - start point to print the space size of each data type.
  *
  * Return: 0 if the program ran successfully
  */
@@ -13,10 +13,10 @@ int main(void)
 	long long int li;
 	float f;
 
-printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
-printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
-printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(l));
-printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(li));
-printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
