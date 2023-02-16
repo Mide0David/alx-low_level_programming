aa74ce847a5532673953a9a03aaacc335d48468d
Mide <midebabalola7@gmail.com>
2023-02-16 12:14:05 -0800

    correct using betty doc syle

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
-
+/**
+ * main - the start point it begin outputting the words in the printf
+ * Return: returns 0 if programs runs successfully
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
