8b72721aaf1072ff5257b8717f3b0e2957cabc40
mide <midebabalola7@gmail.com>
2023-02-17 22:15:57 +0000

    solution to task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/**
+ * main - ouputs the number of two digits
+ * Return: returns  0 if process was successful
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			if (i != j)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+				if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+putchar('\n');
+	return (0);
+}
