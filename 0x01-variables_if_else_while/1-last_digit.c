5e3820df8b09facc829f3beae6bdbece9353fd1d
mide <midebabalola7@gmail.com>
2023-02-17 19:09:09 +0000

    change lastDigit to lstDigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lstDigit);
 	}
-	else if (lstDigit < 6 && !(lastDigit == 0))
+	else if (lstDigit < 6 && !(lstDigit == 0))
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstDigit);
 	}
