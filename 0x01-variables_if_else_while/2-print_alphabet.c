969d51a9f57c51aa5e89a465f8e7d4364a5b5fb0
mide <midebabalola7@gmail.com>
2023-02-17 19:29:52 +0000

    solution for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - ouputs the letter a to z
+ * Return: returns  0 if process was successful
+ */
+int main(void)
+{
+	for (char a2z = 'a'; a2z <= 'z'; a2z++)
+	{
+		putchar(a2z);
+	}
+	return (0);
+}
