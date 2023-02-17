dacedebe9155e2d70fcb31af2bab9db24a9c08b4
mide <midebabalola7@gmail.com>
2023-02-17 20:13:28 +0000

    solution for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - ouputs the number 0 - 9
+ * Return: returns  0 if process was successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+putchar('\n');
+	return (0);
+}
