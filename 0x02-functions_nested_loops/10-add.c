4b9f2d61bf63e1fb82cd88f8442b970d7f688f43
mide <midebabalola7@gmail.com>
2023-02-21 21:59:49 +0000

    solution to task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two arguments
+ * @n1: first parameter
+ * @n2: second parameter
+ * Return: the integer data type
+ *
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
