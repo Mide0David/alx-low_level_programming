3240c7999e9d467acc369a300c1056b26ccac926
mide <midebabalola7@gmail.com>
2023-02-21 17:33:46 +0000

    solution to task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - outputs the letters a to z
+ */
+
+void print_alphabet_x10(void)
+{
+	int y;
+
+	for (y = 0; y < 10; i++)
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
+}
