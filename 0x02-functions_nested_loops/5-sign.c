4637950c21a28b7d34faa829ef15e0a0b6a8e01b
mide <midebabalola7@gmail.com>
2023-02-21 21:08:37 +0000

    solution to task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for lowercase characters
+ * Return: returns 1 if num is postive nd -1 for neg 0 for rest
+ * @n: this holds the character passed in
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	}
+	_putchar(48);
+	return (0);
+	}
+}
