13df21d8893f42fc77a16d8e9fb273cd72e6ca47
mide <midebabalola7@gmail.com>
2023-02-21 19:54:49 +0000

    remove putchar typo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include<unistd.h>
 
 /**
- * main - to output the word _putchar
+ * _putchar - to output the word _putchar
+ * @c: the char to output
  * Return: it returns zero if successful
  */
 int _putchar(char c)
 {
-	putchar('\n');
 	return (write(1, &c, 1));
 }
