fdc849574a4acb295699747247435dcd8fde101b
mide <midebabalola7@gmail.com>
2023-02-21 16:15:00 +0000

    solve function body in header

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet - outputs the letters a to z
  */
-int main(void)
+
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include<stdio.h>
+#include<stdio.>
 
-void print_alphabet(void)
-{
-	char i;
-	
-	for(i = 'a'; i <= 'z'; i++)
-	{
-		putchar(i);
-	}
-}
+void print_alphabet(void);
 #endif
