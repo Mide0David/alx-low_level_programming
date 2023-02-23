e22691c2a91570d59c04b092a20283187ac64aef
mide <midebabalola7@gmail.com>
2023-02-23 22:58:23 +0000

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (((i % 3) == 0) && ((i % 5) == 0))
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("FizzBuzz");
 		}
@@ -29,7 +29,12 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-		printf("\n");
+		if (i == 100)
+		{
+			continue;
+		}
+		printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
