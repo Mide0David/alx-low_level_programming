95431514f59b87e03f6afddf85b66cd0f5bced6d
mide <midebabalola7@gmail.com>
2023-02-23 10:35:17 +0000

    change tenary operator to if statment for betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
 /**
- *
+ * _isupper - identifies the uppercase char
+ * Return: returns 1 if uppecase
+ * returns 0 if lowercase
+ * @c: receives character argument
  *
  */
 int _isupper(int c)
 {
-	return c >= 65 && c <= 90 ? 1 : 0;
+	if ((c >= 65k) && (c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
