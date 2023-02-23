ee24f7e410735f3de53ffe879c4c0c426415ddb7
mide <midebabalola7@gmail.com>
2023-02-23 10:50:38 +0000

    solution to task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - identifies the number
+ * @c: receives the integer argument
+ * Return: returns 1 if it a number
+ * returns 0 if not
+ */
+int _isdigit(int c);
+{
+	if ((a >= 0) && (a <= 9))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
