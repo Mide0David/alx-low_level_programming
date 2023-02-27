f41e3568526de6f650f76d567d6aaea1abfb25ea
mide <midebabalola7@gmail.com>
2023-02-27 15:52:12 +0000

    solution for task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - assigns a new value to the 
- * int its pointing to 
+ * reset_to_98 - assigns a new value to the
+ * int its pointing to
  * @n: parameter to get updated
  */
 void reset_to_98(int *n)
