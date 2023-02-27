a05ac151a8fc9c4a4279bc38236e42f30a6f9266
mide <midebabalola7@gmail.com>
2023-02-27 21:03:14 +0000

    no 5 - 10

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,6 +7,19 @@
  */
 void print_rev(char *s)
 {
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+
+	_putchar('\n');
 	int i;
 	int n;
 
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: it receives the string to be reversed 
+ */
+void rev_string(char *s)
+{
+	int i, j, k;
+	char *a, aux;
+
+	a = s;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+
+	for (k = 1; k < j; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (j / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
+}
