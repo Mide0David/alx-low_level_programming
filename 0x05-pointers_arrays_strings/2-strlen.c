fc0fb8025b1c037055775db119a4159909f42682
mide <midebabalola7@gmail.com>
2023-02-27 21:37:59 +0000

    changes to task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- *
+ * _strlen - count the number of char in string
+ * @s: the string to be used in function
+ * Return: return the string length
  */
 
 int _strlen(char *s)
@@ -10,9 +12,10 @@ int _strlen(char *s)
 
 	len = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
+	return (len);
 }
