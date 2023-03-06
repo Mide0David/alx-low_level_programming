860be09c316cb6577dbd9d10d1df9957f7a1b492
mide <midebabalola7@gmail.com>
2023-03-06 14:07:55 +0000

    create a function dat search for first instance of a char in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - it return the first occurance of
+ * c to the function
+ * @s: the string to search for the occurance
+ * @c: the charater to search for
+ * Return: returns the address of the first occurance
+ * and if it is found or null if it is not in the string
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	char *ptr = s;
+
+	while (*ptr != 0)
+	{
+		if (*ptr == c)
+		{
+			return (s);
+		}
+		ptr++;
+	}
+	return (NULL);
+}
