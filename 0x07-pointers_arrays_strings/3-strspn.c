b85ad56fa07f4296c046896aae203425792822c9
mide <midebabalola7@gmail.com>
2023-03-06 17:14:23 +0000

    solution to task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _strspn - gets the length of a prefix substring
+ * @s:the string to check
+ * @accept: the string to compare
+ * Return: returns the amount of time accept 
+ * char are in s
  */
 unsigned int _strspn(char *s, char *accept)
 {
