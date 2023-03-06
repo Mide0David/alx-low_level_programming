f3c9285c8ee4a1d90565627c856849bbf07487f9
mide <midebabalola7@gmail.com>
2023-03-06 17:16:19 +0000

    correct trailing spaces and add space btwn comma

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,13 +4,13 @@
  * _strspn - gets the length of a prefix substring
  * @s:the string to check
  * @accept: the string to compare
- * Return: returns the amount of time accept 
+ * Return: returns the amount of time accept
  * char are in s
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int k = 0;
-	int i,j;
+	int i, j;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
