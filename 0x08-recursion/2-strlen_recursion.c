e2b52dccbc07810d6771aa48f6164b1b07abf0a2
mide <midebabalola7@gmail.com>
2023-03-08 21:39:42 +0000

    update to 0-2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,14 +9,13 @@
 
 int _strlen_recursion(char *s)
 {
+	int longit = 0;
 
-	if (*s == '\0')
+	if (*s)
 	{
-		return (0);
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
-	else
-	{
-		s++;
-		return (1 + _strlen_recursion(s));
-	}
+
+	return (longit);
 }
