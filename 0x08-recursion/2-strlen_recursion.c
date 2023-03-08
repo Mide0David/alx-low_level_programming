7d8a410c75da53f1e6d3c84c651ad7aa3753c69e
mide <midebabalola7@gmail.com>
2023-03-08 22:23:20 +0000

    correct betty doc style error

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,18 +4,18 @@
  * _strlen_recursion - counts the amounts of strings
  * in a array
  * @s: the array to be counted
- * Returns: length of string
+ * Return: length of string
  */
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0' )
+	if (*s == '\0')
 	{
 		return (len);
 	}
 	else
 	{
 		s++;
-		return (1 + _strlen_recursion(s);
+		return (1 + _strlen_recursion(s));
 	}
 }
