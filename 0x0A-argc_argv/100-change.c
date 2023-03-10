4b7e61cc8afc96cf8de52d393c5b72f8cc7b7a7f
mide <midebabalola7@gmail.com>
2023-03-10 22:51:44 +0000

    solution to task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,88 +1,56 @@
 #include <stdio.h>
-		
 #include <stdlib.h>
-		
 #include "main.h"
-		
 
-		
 /**
-		
  * main - prints the minimum number of coins to
-		
  * make change for an amount of money
-		
  * @argc: number of arguments
-		
  * @argv: array of arguments
-		
  *
-		
  * Return: 0 (Success), 1 (Error)
-		
  */
-		
+
 int main(int argc, char *argv[])
-		
 {
-		
-	int num, j, result;
-		
-	int coins[] = {25, 10, 5, 2, 1};
-		
+	int cents, num_one = 0, num_quarters = 0, cents = atoi(argv[1]);
+	int num_dimes = 0, num_nickels = 0, num_twos = 0;
+	int total_coins = num_quarters + num_dimes + num_nickels + num_twos + num_one;
 
-		
 	if (argc != 2)
-		
 	{
-		
-		printf("Error\n");
-		
-		return (1);
-		
+		printf("Error\n"), return (1);
 	}
-		
-
-		
-	num = atoi(argv[1]);
-		
-	result = 0;
-		
-
-		
-	if (num < 0)
-		
+	if (cents < 0)
 	{
-		
 		printf("0\n");
-		
 		return (0);
-		
 	}
-		
-
-		
-	for (j = 0; j < 5 && num >= 0; j++)
-		
+	while (cents >= 25)
 	{
-		
-		while (num >= coins[j])
-		
-		{
-		
-			result++;
-		
-			num -= coins[j];
-		
-		}
-		
+		cents -= 25;
+		num_quarters++;
 	}
-		
-
-		
-	printf("%d\n", result);
-		
+	while (cents >= 10)
+	{
+		cents -= 10;
+		num_dimes++;
+	}
+	while (cents >= 5)
+	{
+		cents -= 5;
+		num_nickels++;
+	}
+	while (cents >= 2)
+	{
+		cents -= 2;
+		num_twos++;
+	}
+	while (cents >= 1)
+	{
+		cents -= 1;
+		num_one++;
+	}
+	printf("%d\n", total_coins);
 	return (0);
-		
 }
-
