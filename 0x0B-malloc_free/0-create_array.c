c51c58290055ca9783bfd9cec876293b422fe93f
Mide <midebabalola7@gmail.com>
2023-03-14 01:36:12 -0700

    fix header issue

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,14 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ptr;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	ptr = (char *) malloc(size * sizeof(char));
 
-	if ((size = 0) || (ptr == NULL))
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
