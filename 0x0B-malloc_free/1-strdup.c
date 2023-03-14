2c4fb020566deeec3ae43ea18c5f64d2184ec11c
mide <midebabalola7@gmail.com>
2023-03-14 09:31:12 +0000

    add space for \0

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,7 +5,7 @@
 /**
  * _strdup - duplicate a string to a new array
  * @str: the string to copy
- * Return: returns the pointer to the copied string
+<F7> * Return: returns the pointer to the copied string
  * and returns NULL if memory allocation falied
  */
 
@@ -14,12 +14,17 @@ char *_strdup(char *str)
 	int i, j, sum = 0;
 	char *cpy;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; *(str + i) != '\0'; i++)
 	{
 		sum++;
 	}
 
-	cpy = (char *) malloc(sum * sizeof(char));
+	cpy = (char *) malloc(sum * sizeof(char) + 1);
 
 	if (cpy == NULL)
 	{
