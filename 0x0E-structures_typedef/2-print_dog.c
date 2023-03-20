c2af2d73ebfef3e1ffe26223f1afb999ac79d51c
mide <midebabalola7@gmail.com>
2023-03-20 21:53:29 +0000

    remove * when assigning to the element

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,18 +9,10 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
-	if (d->name == NULL)
-	{
-		d->name = "(nil)";
-	}
-	if (d->owner == NULL)
-	{
-		d->owner = "(nil)";
-	}
-
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->name, d->age, d->owner);
 }
