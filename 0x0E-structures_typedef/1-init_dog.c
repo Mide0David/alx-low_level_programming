73b9bee3c5329de2889634705d2f6ed1b5a5f65d
mide <midebabalola7@gmail.com>
2023-03-20 21:45:23 +0000

    solution of all structure and tyedef task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes the element in init_dog
+ * @d: the container to initialize
+ * @name: the value to initialize the char type
+ * @age: the value for the int type in the container
+ * @owner: the value for the 2nd char type
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d = malloc(sizeof(d));
+
+		d-<F9>> name = *name;
+		d-> age = age;
+		d-> owner = *owner;
+	}
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print element struct dog
+ * @d: pointer to struct dog
+ * Return: return pointer to struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->name, d->age, d->owner);
+}
