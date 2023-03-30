0f6469580338600bc949a578b648790380fe9afe
mide <midebabalola7@gmail.com>
2023-03-30 19:31:17 +0000

    solution to task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - print the list
+ * @h: the list to print from
+ * Return: the number of elemet int the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
