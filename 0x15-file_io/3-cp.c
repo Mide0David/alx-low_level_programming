34b86e4c081bc8af2ac9a0ac359d563798e685ab
mide <midebabalola7@gmail.com>
2023-05-08 23:58:57 +0000

    fixed code fail

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,23 +24,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		perror("Usage");
-		dprintf(STDERR_FILENO, "cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
-		perror("Error");
-		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	fd_value = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, permissions);
 	if (fd_value == -1)
 	{
-		perror("Error");
-		dprintf(STDERR_FILENO, "Can't read from %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[2]);
 		exit(99);
 	}
 	while (nread > 0)
@@ -48,27 +45,23 @@ int main(int argc, char *argv[])
 		nread = read(fd, buffer, BUF_SIZE);
 		if (write(fd_value, buffer, nread) != nread)
 		{
-			perror("Error");
-			dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(98);			
 		}
 	}
 	if (nread == -1)
 	{
-		perror("Error");
-		dprintf(STDERR_FILENO, "Can't read form file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read form file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(fd) == -1)
 	{
-		perror("Error");
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 	if (close(fd_value) == -1)
 	{
-		perror("Error");
-		dprintf(STDERR_FILENO, "Can't close fd %d", fd_value);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_value);
 		exit(100);
 	}
 	return (0);
