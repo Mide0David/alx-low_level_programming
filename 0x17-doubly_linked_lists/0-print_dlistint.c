588e82a82d54a1113ce229f5e71876a780e1115f
mide <midebabalola7@gmail.com>
2023-06-15 22:19:31 +0000

    print all the elements of a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,9 +13,6 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 	const dlistint_t *temp = h;
 
-	if (h == NULL)
-		return (-1);
-
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
